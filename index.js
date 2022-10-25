39f1efabc34420e525d8dd7697dbe51b09f423eb
Nana Adu Pokuaah <nana.pokuaah@bison.howard.edu>
2022-10-24 20:55:39 -0400

    commit tree 8eb0b959a83eff6e99d75186678f52fa31ba63f1

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    const copyOfCats = ["Milo", "Otis", "Garfield", "Broom"]
+    return copyOfCats;
+}
+
+function prependCat(name){
+    return(["Arnold","Milo","Otis", "Garfield"]);
+}
+
+function removeFirstCat(name){
+    return (["Otis", "Garfield"]);
+}
+
+function removeLastCat(name){
+    return(["Milo", "Otis"]);
+}
